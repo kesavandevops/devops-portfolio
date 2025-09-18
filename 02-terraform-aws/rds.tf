e6304661832fdbd4ffe39bbc35f9cebb46ef850b
Kesavan <kesavaragav@gmail.com>
2025-09-18 16:35:37 +0530

    Update rds.tf

--- 02-terraform-aws/rds.tf
+++ 02-terraform-aws/rds.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "rds_sg" {
     from_port       = 3306
     to_port         = 3306
     protocol        = "tcp"
-    security_groups = [aws_security_group.ec2_sg.id] # EC2 SG we defined earlier
+    security_groups = [aws_security_group.web_sg.id] # EC2 SG we defined earlier
   }
 
   egress {
