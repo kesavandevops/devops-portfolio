e875c07fd499ed7d44883fd486f456915dd712c4
Kesavan <kesavaragav@gmail.com>
2025-09-25 07:46:29 +0530

    Update worker.py

--- 04-docker-microservices/worker/worker.py
+++ 04-docker-microservices/worker/worker.py
@@ -7,14 +7,14 @@ redis_host = os.getenv("REDIS_HOST", "redis")
 redis_port = int(os.getenv("REDIS_PORT", 6379))
 r = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
 
-print("Worker service started... waiting for jobs.")
+print("Worker service started... waiting for jobs.", flush=True)
 
 while True:
-    task = r.lpop("tasks")  # get a task from Redis
-    if task:
-        print(f"Processing task: {task}")
-        # simulate work
-        time.sleep(2)
-        print(f"Completed task: {task}")
-    else:
+    try:
+        queue, task = r.brpop("tasks", timeout=0)  # block until task arrives
+        print(f"Processing task: {task}", flush=True)
+        time.sleep(2)  # simulate work
+        print(f"Completed task: {task}", flush=True)
+    except Exception as e:
+        print(f"Error: {e}", flush=True)
         time.sleep(1)
