daf3f108a9b1f6aa4761658e29983ab7fae9cb5a
Kesavan <kesavaragav@gmail.com>
2025-09-26 13:53:24 +0530

    Update iam.tf

--- 05-serverless-aws/terraform/iam.tf
+++ 05-serverless-aws/terraform/iam.tf
@@ -1,1 +1,50 @@
+# Lambda Exec role
+resource "aws_iam_role" "lambda_exec" {
+  name = "${var.project_name}-lambda-exec"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+
+  tags = var.tags
+}
+
+# Attach AWS managed policy for logs
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Inline policy to allow DynamoDB access to the specific table
+resource "aws_iam_role_policy" "lambda_dynamodb_policy" {
+  name = "${var.project_name}-dynamodb-policy"
+  role = aws_iam_role.lambda_exec.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:Query",
+          "dynamodb:Scan",
+          "dynamodb:UpdateItem"
+        ]
+        Resource = [
+          aws_dynamodb_table.tasks.arn,
+          "${aws_dynamodb_table.tasks.arn}/*"
+        ]
+      }
+    ]
+  })
+}
