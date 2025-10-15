7e870c43ace16d18e5212ab823c3382e77072e79
Kesavan <kesavaragav@gmail.com>
2025-10-15 19:54:51 +0530

    Update provider.tf

--- 05-serverless-aws/terraform/provider.tf
+++ 05-serverless-aws/terraform/provider.tf
@@ -6,6 +6,14 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "my-terraform-state-file-ap-south-1"  # <-- Replace
+    key            = "serverless-api/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock-table"  # <-- Replace
+    encrypt        = true
+  }
 }
 
 provider "aws" {
