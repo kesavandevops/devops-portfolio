b97ee66854aff5f8db5b762206b18a423679be64
Kesavan <kesavaragav@gmail.com>
2025-10-16 21:29:36 +0530

    Update provider.tf

--- 05-serverless-aws/terraform/provider.tf
+++ 05-serverless-aws/terraform/provider.tf
@@ -1,20 +1,12 @@
 terraform {
   required_version = ">= 1.2.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
-
-  backend "s3" {
-    bucket         = "my-terraform-state-file-ap-south-1"  # <-- Replace
-    key            = "serverless-api/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock-table"  # <-- Replace
-    encrypt        = true
-  }
-}
 
 provider "aws" {
   region = var.aws_region
